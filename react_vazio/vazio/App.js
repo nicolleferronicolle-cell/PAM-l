bd704303323406c40eacb72e1b82548b6a7dbc25
nicolleferronicolle-cell <nicolleferronicolle@gmail.com>
2026-06-02 17:56:34 +0000

    cant catch me now

--- react_vazio/vazio/App.js
+++ react_vazio/vazio/App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.titulo}>
+        <Text> Can`t Catch Me Now</Text>
+      </View>
+      <View style={styles.cont_texto}>
+        <Text> 
+          There's blood on the side of the mountain
+          There's writing all over the wall
+          Shadows of us are still dancing
+          In every room and every hall
+        </Text>
+      </View> 
+      <View> 
+      
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  titulo: {
     flex: 1,
-    backgroundColor: '#fff',
+
+    backgroundColor: '#f09c1e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  cont_texto: {
+    width:100,
+    
+  },
 });
+
