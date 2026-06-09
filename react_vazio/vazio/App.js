5f0beb124c3a020a84bd3ef2c28222db8441ba23
nicolleferronicolle-cell <nicolleferronicolle@gmail.com>
2026-06-09 17:55:00 +0000

    plantas

--- react_vazio/vazio/App.js
+++ react_vazio/vazio/App.js
@@ -4,15 +4,98 @@ import { StyleSheet, Text, View, ScrollView } from 'react-native';
 export default function App() {
   return (
     <ScrollView>
-      <View style={styles.titulo}>
-        <Text> Can`t Catch Me Now</Text>
+     
+      <View style={styles.titulo}> 
+        <Text> <h1>Can`t Catch Me Now
+         
+          </h1> </Text>
       </View>
+       <View style={styles.titulo}>
+        <text> <h3>olivia Rodrigo</h3></text> 
+       </View>
+
       <View style={styles.cont_texto}>
         <Text> 
-          There's blood on the side of the mountain
-          There's writing all over the wall
-          Shadows of us are still dancing
-          In every room and every hall
+        <View style={{ width: '100%', alignItems: 'center', justifyContent: 'center', alignSelf: 'center', marginVertical: 20}}>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>There's blood on the side of the mountain</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>There's writing all over the wall</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>Shadows of us are still dancing</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>In every room and every hall</Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}></Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>There's snow falling over the city</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>You thought that it would wash away</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>The bitter taste of my fury</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>And all of the messes you made</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>Yeah, you think that you got away</Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}></Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>But I'm in the trees, I'm in the breeze</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>My footsteps on the ground</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>You'll see my face in every place</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>But you can't catch me now</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>Through wading grass, the months will pass</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>You'll feel it all around</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>I'm here, I'm there, I'm everywhere</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>But you can't catch me now</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>No, you can't catch me now</Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}></Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>Bet you thought I'd never do it</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>Thought it'd go over my head</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>I bet you figured I'd pass with the winter</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>Be something easy to forget</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>Oh, you think I'm gone 'cause I left</Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}></Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>But I'm in the trees, I'm in the breeze</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>My footsteps on the ground</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>You'll see my face in every place</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>But you can't catch me now</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>Through wading grass, the months will pass</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>You'll feel it all around</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>I'm here, I'm there, I'm everywhere</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>But you can't catch me now</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>No, you can't catch me now</Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}></Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>Ooh, ooh, ooh, ooh</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>Ooh, ooh, ooh, ooh, ooh, ooh</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>Ooh, ooh, ooh, ooh</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>Ooh, ooh, ooh, ooh, ooh, ooh</Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}></Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>You, you can't, you can't catch me now</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>I'm coming like a storm into your town</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>You can't, you can't catch me now</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>I'm higher than the hopes that you brought down</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>You can't, you can't catch me now</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>I'm coming like a storm into your town</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>You can't, you can't catch me now</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>I'm higher than the hopes that you brought down</Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}></Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>You can't, you can't catch me now</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>I'm coming like a storm into your town</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>You can't, you can't catch me now</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>You can't, you can't, you can't</Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}></Text>
+
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>There's blood on the side of the mountain</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>It's turning a new shade of red</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>Yeah, sometimes the fire you founded</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>Don't burn the way you'd expect</Text>
+        <Text style={{ textAlign: 'center', width: '100%', marginVertical: 4 }}>Yeah, you thought that this was the end</Text>
+    
+      </View>
         </Text>
       </View> 
       <View> 
@@ -25,14 +108,16 @@ export default function App() {
 const styles = StyleSheet.create({
   titulo: {
     flex: 1,
-
-    backgroundColor: '#f09c1e',
+    backgroundColor: '#e48900',
     alignItems: 'center',
     justifyContent: 'center',
   },
   cont_texto: {
-    width:100,
+    width:1500,
+    backgroundColor: '#e48900',
     
   },
+  
 });
+
 
